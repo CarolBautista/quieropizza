21c85673341a114aae8a8237e7268d62b603268c
carolBautista <angiecb1347@gmail.com>
2019-03-21 01:44:26 -0500

    Tarea

---
+++ QuieroPizza/EducacionAvanzada/EducacionAvanzada.WebAdmin/Controllers/NotasDetalleController.cs
@@ -0,0 +1,80 @@
+﻿using EducacionAvanzada.BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EducacionAvanzada.WebAdmin.Controllers
+{
+    public class NotasDetalleController : Controller
+
+    {
+        NotasBL _notaBL;
+        AlumnosBL _alumnosBL;
+        materiaBL _MateriaBL;
+        
+
+        public NotasDetalleController()
+        {
+            _notaBL = new NotasBL();
+            _alumnosBL = new AlumnosBL();
+            _MateriaBL = new materiaBL();
+        }
+        // GET: NotasDetalle
+        public ActionResult Index(int id)
+        {
+            var Notas = _notaBL.ObtenerNota(id);
+            Notas.ListadeNotasDetalle = _notaBL.ObtenerNotasDetalle(id);
+            return View(Notas);
+        }
+
+        public ActionResult Crear(int id)   
+        {
+            var nuevaNotasDetalle = new NotasDetalle();
+            nuevaNotasDetalle.NotaId = id;
+
+            var alumnos = _alumnosBL.ObtenerAlumnos();
+            ViewBag.AlumnoId = new SelectList(alumnos, "Id", "Nombre");
+
+            var materias = _MateriaBL.Obtenermaterias();
+            ViewBag.MateriaId = new SelectList(materias, "Id", "Descripcion");
+
+            return View(nuevaNotasDetalle);
+        }
+
+        [HttpPost]
+        public ActionResult Crear(NotasDetalle notasDetalle)
+        {
+            if(ModelState.IsValid)
+            {
+                if(notasDetalle.AlumnoId == 0)
+                {
+                    ModelState.AddModelError("AlumnoId", "Seleccione un Alumno");
+                    return View(notasDetalle);
+                }
+
+                _notaBL.GuardarNotasDetalle(notasDetalle);
+                return RedirectToAction("Index", new { id = notasDetalle.NotaId });
+            }
+            var alumnos = _alumnosBL.ObtenerAlumnos();
+            ViewBag.AlumnoId = new SelectList(alumnos, "Id", "Nombre");
+
+            return View(notasDetalle);
+        }
+
+        public ActionResult Eliminar(int id)
+        {
+            var notaDetalle = _notaBL.ObtenerNotasDetalleporId(id);
+
+            return View(notaDetalle);
+        }
+        [HttpPost]
+        public ActionResult Eliminar(NotasDetalle notaDetalle)
+        {
+            _notaBL.EliminarNotaDetalle(notaDetalle.Id);
+            return RedirectToAction("Index", new { id = notaDetalle.NotaId });
+        }
+    }
+    
+}
